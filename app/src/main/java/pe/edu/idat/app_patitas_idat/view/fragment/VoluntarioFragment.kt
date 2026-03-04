85c844157b90d4a1dd52b747ccc113fbe2048edb
idatsalon54-log <idatsalon54@gmail.com>
2026-03-04 09:54:04 -0500

    fragments terminados

---
+++ app/src/main/java/pe/edu/idat/app_patitas_idat/view/fragment/VoluntarioFragment.kt
@@ -0,0 +1,19 @@
+package pe.edu.idat.app_patitas_idat.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pe.edu.idat.app_patitas_idat.R
+
+class VoluntarioFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_voluntario, container, false)
+    }
+
+}
