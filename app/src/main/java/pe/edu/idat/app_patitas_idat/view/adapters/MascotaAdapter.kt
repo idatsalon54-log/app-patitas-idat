15097203d40db792a064f56d5175eb75c31f72b7
idatsalon54-log <idatsalon54@gmail.com>
2026-03-11 09:54:22 -0500

    adapter y layout de reciclerview

--- app/src/main/java/pe/edu/idat/app_patitas_idat/view/adapters/MascotaAdapter.kt
+++ app/src/main/java/pe/edu/idat/app_patitas_idat/view/adapters/MascotaAdapter.kt
@@ -1,4 +1,31 @@
 package pe.edu.idat.app_patitas_idat.view.adapters
 
-class MascotaAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import pe.edu.idat.app_patitas_idat.databinding.ItemMascotaBinding
+import pe.edu.idat.app_patitas_idat.retrofit.response.ResponseMascota
+
+class MascotaAdapter (private var listMascotas: List<ResponseMascota>)
+    : RecyclerView.Adapter<MascotaAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemMascotaBinding)
+        : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MascotaAdapter.ViewHolder {
+        val binding = ItemMascotaBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MascotaAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount() = listMascotas.size
+
+
 }
