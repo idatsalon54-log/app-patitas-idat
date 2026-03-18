a72638fecf873b5ecf0ecfbfe964a18cb17ebf5d
idatsalon54-log <idatsalon54@gmail.com>
2026-03-18 08:36:26 -0500

    consumo de API Rest, para el listado de mascotas

--- app/src/main/java/pe/edu/idat/app_patitas_idat/view/adapters/MascotaAdapter.kt
+++ app/src/main/java/pe/edu/idat/app_patitas_idat/view/adapters/MascotaAdapter.kt
@@ -3,6 +3,8 @@ package pe.edu.idat.app_patitas_idat.view.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import okhttp3.internal.wait
 import pe.edu.idat.app_patitas_idat.databinding.ItemMascotaBinding
 import pe.edu.idat.app_patitas_idat.retrofit.response.ResponseMascota
 
@@ -22,7 +24,18 @@ class MascotaAdapter (private var listMascotas: List<ResponseMascota>)
     }
 
     override fun onBindViewHolder(holder: MascotaAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        with(holder){
+            with(listMascotas[position]){
+                binding.tvnommascota.text = nommascota
+                binding.tvcontacto.text = contacto
+                binding.tvfecha.text = fechaperdida
+                binding.tvlugar.text = lugar
+                urlimagen = "https://imoments.pe/apps"+urlimagen.substring(24)
+                Glide.with(holder.itemView.context)
+                    .load(urlimagen)
+                    .into(binding.ivmascota)
+            }
+        }
     }
 
     override fun getItemCount() = listMascotas.size
--- app/src/main/java/pe/edu/idat/app_patitas_idat/view/fragment/HomeFragment.kt
+++ app/src/main/java/pe/edu/idat/app_patitas_idat/view/fragment/HomeFragment.kt
@@ -5,17 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import pe.edu.idat.app_patitas_idat.R
+import pe.edu.idat.app_patitas_idat.databinding.FragmentHomeBinding
+import pe.edu.idat.app_patitas_idat.view.adapters.MascotaAdapter
+import pe.edu.idat.app_patitas_idat.viewmodel.MascotaViewModel
 
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mascotaViewModel: MascotaViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater,
+            container, false)
+        binding.rvmascota.layoutManager = LinearLayoutManager(
+            requireActivity())
+        mascotaViewModel = ViewModelProvider(requireActivity())
+            .get(MascotaViewModel::class.java)
+        listarMascotas()
+        return binding.root
+    }
+
+    fun listarMascotas(){
+        mascotaViewModel.listarMascotas()
+            .observe(viewLifecycleOwner,
+                Observer{
+                    listMascota ->
+                binding.rvmascota.adapter = MascotaAdapter(listMascota)
+            })
     }
 
 }
